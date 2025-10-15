1b10f0f47317de721c6422231bf51e773816005f
AmirRezakhosraviyan84 <khosraviyanamirreza1384@gmail.com>
2025-10-15 18:38:36 +0330

    Update 0007_alter_userotp_code.py

--- accounts/migrations/0007_alter_userotp_code.py
+++ accounts/migrations/0007_alter_userotp_code.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0006_customuser_full_name'),
+        ('accounts', '0006_fix_missing_0006.py'),
     ]
 
     operations = [
