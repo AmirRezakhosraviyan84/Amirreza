f1facb13611ede0dda118fc269f19148c6bebcea
AmirRezakhosraviyan84 <khosraviyanamirreza1384@gmail.com>
2025-10-15 16:32:27 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'amirreza_db',
+        'USER': 'amir',
+        'password':'123456',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
